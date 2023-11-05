c6fd91bfe5a8da18eab9a8e337f1420e9771f628
enser <mammadva@code.edu.az>
2023-11-05 13:05:05 +0400

    Добавьте файлы проекта.

---
+++ 05.11.2023 hw/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int n = 34;
+//int m = 55;
+
+//int sum = SumBet(n, m);
+
+//Console.WriteLine(sum);
+//static int SumBet(int n, int m)
+//{
+//    int sum = 0;
+//    for (int i = n; i <= m; i++)
+//    {
+//        if (i % 2 != 0)
+//        {
+//            sum += i;
+//        }
+//    }
+//    return sum;
+//}
+
+//int[] num = { 32,5,68,12,3,98,2,64,67,11 };
+
+//int sum = SumInArray(num);
+
+//Console.WriteLine(sum);
+//static int SumInArray(int[] array)
+//{
+//    int sum = 0;
+//    foreach (int num in array)
+//    {
+//        if (num%2 != 0)
+//        {
+//            sum+= num;
+//        }
+//    }
+//    return sum;
+//}
+
+//int[] num = { 12,3,64,23,6,97,4,97,34,61 };
+
+//int count = CountInArray(num);
+
+//Console.WriteLine(count);
+//static int CountInArray(int[] array)
+//{
+//    int count = 0;
+//    foreach (int num in array)
+//    {
+//        if (num % 2 == 0)
+//        {
+//            count++;
+//        }
+//    }
+//    return count;
+//}
+
